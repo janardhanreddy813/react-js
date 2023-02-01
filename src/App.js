8b02cca99fbc1e8970623c999b71461ecc26b46a
janardhan <janardhanravula813@gmail.com>
2023-02-01 18:43:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return ( <div>
+        <h3 style={{textAlign:"center"}}> Welcome to react tutorial </h3>
+         </div>
+    )
+}
+
+export default App
---
+++ src/CompA.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CompA = () => {
+    return ( <div>
+
+        </div>
+    )
+}
+
+export default CompA
