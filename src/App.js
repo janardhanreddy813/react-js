b5c1236a20b2dcc69a62fbaf49747fdd64bcc37f
janardhan <janardhanravula813@gmail.com>
2023-02-01 20:12:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return ( <div>
-        <h3 style={{textAlign:"center"}}> Welcome to RJR </h3>
+        <h3 style={{textAlign:"center"}}> Welcome to React technologies </h3>
          </div>
     )
 }
