cc6e3ebcf51e640ba446a547431c34214c072bc3
janardhan <janardhanravula813@gmail.com>
2023-02-01 19:13:51 +0530

    text changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return ( <div>
-        <h3 style={{textAlign:"center"}}> Welcome to react tutorial </h3>
+        <h3 style={{textAlign:"center"}}> Welcome to RJR </h3>
          </div>
     )
 }
